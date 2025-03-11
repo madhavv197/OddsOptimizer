80798035e40baf1544fa25bee0a50ff4f5224fb1
Madhav Venkatesh <madhavv@Madhavs-MacBook-Pro.local>
2025-03-11 12:57:04 +0100

    Stopped tracking deprecated files

--- utils/calc_ev_outcome.py
+++
@@ -1,6 +0,0 @@
-def calculate_ev(prob, odds):
-    """
-    Given a probability (prob) and decimal odds,
-    returns the expected value of a single 1-unit stake.
-    """
-    return prob * (odds - 1) - (1 - prob)
